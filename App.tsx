686bb4f173959ea4d29d40e8ce0aa981f57d8046
Guillermo <grobles64@ieszaidinvergeles.org>
2024-12-11 18:21:05 +0100

    Implementación FlatList

--- App.tsx
+++ App.tsx
@@ -1,16 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import { SafeAreaView, StatusBar, StyleSheet, View, Text, Pressable } from 'react-native';
+import { SafeAreaView, StatusBar, StyleSheet, FlatList, Text, Pressable } from 'react-native';
 
 type Country = {
   region: string;
 };
 
-function onPressFunction(cadena: string) {
-  console.log(cadena);
-}
-
 const App = (): React.JSX.Element => {
   const [regions, setRegions] = useState<string[]>([]);
+
   async function getRegions() {
     const response = await fetch('https://restcountries.com/v3.1/all?fields=region');
     if (!response.ok) {
@@ -19,28 +16,31 @@ const App = (): React.JSX.Element => {
     const jsonData: Country[] = await response.json();
     const regionsResponse = [...new Set(jsonData.map((country) => country.region))];
     setRegions(regionsResponse);
-
   }
   useEffect(() => {
     getRegions();
   }, []);
 
+  const onPressFunction = (region: string) => {
+    console.log(region);
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar barStyle="dark-content" />
-      <View>
-        {regions.length > 0 ? (
-          regions.map((region, index) => (
-            <Pressable key={index} onPress={() => onPressFunction(region)}>
-              <Text style={styles.regionText}>
-                {region}
-              </Text>
+      {regions.length > 0 ? (
+        <FlatList
+          data={regions}
+          keyExtractor={(item, index) => index.toString()}
+          renderItem={({ item }) => (
+            <Pressable onPress={() => onPressFunction(item)}>
+              <Text style={styles.regionText}>{item}</Text>
             </Pressable>
-          ))
-        ) : (
-          <Text>Cargando regiones...</Text>
-        )}
-      </View>
+          )}
+        />
+      ) : (
+        <Text>Cargando regiones...</Text>
+      )}
     </SafeAreaView>
   );
 };
@@ -58,5 +58,6 @@ const styles = StyleSheet.create({
 });
 
 export default App;
+
 
 
