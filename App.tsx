a1015f244a87f4e744c1d4cf718007db87b3b5fc
Guillermo <grobles64@ieszaidinvergeles.org>
2024-12-11 18:17:57 +0100

    onPressFunction(region)

--- App.tsx
+++ App.tsx
@@ -1,24 +1,27 @@
 import React, { useEffect, useState } from 'react';
-import { SafeAreaView, StatusBar, StyleSheet, View, Text } from 'react-native';
+import { SafeAreaView, StatusBar, StyleSheet, View, Text, Pressable } from 'react-native';
 
 type Country = {
   region: string;
 };
 
+function onPressFunction(cadena: string) {
+  console.log(cadena);
+}
+
 const App = (): React.JSX.Element => {
   const [regions, setRegions] = useState<string[]>([]);
-  useEffect(() => {
-    async function getRegions() {
-      const response = await fetch('https://restcountries.com/v3.1/all?fields=region');
-      if (!response.ok) {
-        throw new Error('Error fetching regions');
-      }
-      const jsonData: Country[] = await response.json();
-      const regionsResponse = [...new Set(jsonData.map((country) => country.region))];
-      setRegions(regionsResponse);
-
+  async function getRegions() {
+    const response = await fetch('https://restcountries.com/v3.1/all?fields=region');
+    if (!response.ok) {
+      throw new Error('Error fetching regions');
     }
+    const jsonData: Country[] = await response.json();
+    const regionsResponse = [...new Set(jsonData.map((country) => country.region))];
+    setRegions(regionsResponse);
 
+  }
+  useEffect(() => {
     getRegions();
   }, []);
 
@@ -28,9 +31,11 @@ const App = (): React.JSX.Element => {
       <View>
         {regions.length > 0 ? (
           regions.map((region, index) => (
-            <Text key={index} style={styles.regionText}>
-              {region}
-            </Text>
+            <Pressable key={index} onPress={() => onPressFunction(region)}>
+              <Text style={styles.regionText}>
+                {region}
+              </Text>
+            </Pressable>
           ))
         ) : (
           <Text>Cargando regiones...</Text>
