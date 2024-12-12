bf8be694365829e5edb41de71c00e93da658a4e3
Guillermo <grobles64@ieszaidinvergeles.org>
2024-12-12 10:03:34 +0100

    CountriesFlatList

--- CountriesScreen.tsx
+++ CountriesScreen.tsx
@@ -2,19 +2,14 @@ import React from 'react';
 import { View, Text, StyleSheet, FlatList } from 'react-native';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { RootStackParamList } from './App';
+import { CountriesFlatList } from './CountriesFlatList';
 
 type Props = NativeStackScreenProps<RootStackParamList, 'Countries'>;
 
 export function CountriesScreen({ route }: Props): React.JSX.Element {
     const { countries } = route.params;
     return (
-        <View style={styles.container}>
-            <FlatList
-                data={countries}
-                keyExtractor={(item, index) => index.toString()}
-                renderItem={({ item }) => <Text style={styles.countryName}>{item.name}</Text>}
-            />
-        </View>
+        <CountriesFlatList countries={countries} />
     );
 }
 const styles = StyleSheet.create({
